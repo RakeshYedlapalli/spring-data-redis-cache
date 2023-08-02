d9de3b9e7d73aac6744c18c46189897a5593e0ce
RakeshYedlapalli <rak.mdg94@gmail.com>
2023-08-02 15:35:39 +0530

    initial commit

---
+++ src/main/java/com/javatechie/redis/SpringDataRedisExampleApplication.java
@@ -0,0 +1,21 @@
+package com.javatechie.redis;
+
+import com.javatechie.redis.entity.Product;
+import com.javatechie.redis.respository.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@SpringBootApplication
+public class SpringDataRedisExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRedisExampleApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/javatechie/redis/controller/ProductController.java
@@ -0,0 +1,44 @@
+package com.javatechie.redis.controller;
+
+import com.javatechie.redis.entity.Product;
+import com.javatechie.redis.respository.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("product")
+@EnableCaching
+public class ProductController {
+
+    @Autowired
+    private ProductDao dao;
+
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        return dao.save(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return dao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @Cacheable(key = "#id",value = "Product",unless = "#result.price > 1000")
+    public Product findProduct(@PathVariable int id) {
+        return dao.findProductById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @CacheEvict(key = "#id",value = "Product")
+    public String remove(@PathVariable int id) {
+        return dao.deleteProduct(id);
+    }
+
+
+}
